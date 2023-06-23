1da25bc14c509431158dfaa380f2742aaa1f62e7
Опарин Артур <troon89@mail.ru>
2023-06-24 00:50:38 +0500

    getJsonParsingTest

--- src/test/java/GetTests.java
+++ src/test/java/GetTests.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,15 @@ public class GetTests {
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void getJsonParsingTest(){
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        String secondMessage = response.get("messages.message[1]");
+        System.out.println(secondMessage);
     }
 }
