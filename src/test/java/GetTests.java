9d975df936f72b522706b145f2ea61155cb1e77f
Опарин Артур <troon89@mail.ru>
2023-06-25 20:23:45 +0500

    getLongRedirectHeaderTest

--- src/test/java/GetTests.java
+++ src/test/java/GetTests.java
@@ -4,6 +4,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class GetTests {
+
+    private int statusCode;
+
     @Test
     public void getTextTest(){
         Response response = RestAssured
@@ -32,5 +35,36 @@ public class GetTests {
                 .andReturn();
         String locationHeaders = response.getHeader("Location");
         System.out.println(locationHeaders);
+    }
+
+    @Test
+    public void getLongRedirectHeaderTest(){
+
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int redirectCount = 0;
+        int statusCode = this.statusCode;
+
+        while(statusCode != 200){
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(url)
+                    .andReturn();
+            String locationHeaders = response.getHeader("Location");
+            statusCode = response.statusCode();
+            if (statusCode == 301){
+                System.out.println("url: " + locationHeaders);
+                url = locationHeaders;
+                redirectCount++;
+            }
+            else if (statusCode == 200) {
+                System.out.println("Number of redirects: " + redirectCount);
+            }
+            else if(statusCode !=200 && statusCode !=301){
+                System.out.println("Unexpected status code: " + statusCode);
+                break;
+            }
+        }
     }
 }
